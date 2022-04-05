ed150491d2d0c2fd67acb85c18a6262d3721f2b5
Thomas Kinsella <thomasjkinsella4@gmail.com>
2022-04-05 14:58:41 -0400

    commit tree 65cf214ccc15cba9b7b34c4a449c69ba4bfd35e0

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+
+class Anagram
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        array.filter do |array_word| 
+            array_word.chars.sort == @word.chars.sort
+        end
+    end
+end
